fc3a705ee5930be0c52d3bceef763b79fc1d3fc4
Henrik <henrik789@gmail.com>
2019-05-02 13:02:48 +0200

    Initial Commit

---
+++ RiksdagsMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiksdagsMemo
+//
+//  Created by Henrik on 2019-05-02.
+//  Copyright © 2019 Henrik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
