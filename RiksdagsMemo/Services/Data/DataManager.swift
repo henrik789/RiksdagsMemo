cc9b3c8cd472bf4ae92ccc17442a9b18ae8425f7
Henrik <henrik789@gmail.com>
2019-05-14 14:07:10 +0200

    improvements

--- RiksdagsMemo/Services/Data/DataManager.swift
+++ RiksdagsMemo/Services/Data/DataManager.swift
@@ -7,7 +7,7 @@ private let badResponseError = NSError(domain: "Bad network response", code: 2, 
 
 class  DataManager {
     var personList = [Person]()
-    var person = Person()
+    
     let networkService: NetworkService
     let jsonUrlString = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista="
     
@@ -25,20 +25,8 @@ class  DataManager {
                 let json = JSON(value)
                 print(json["personlista"]["@hits"].intValue)
                 for index in 0..<json["personlista"]["@hits"].intValue {
-                    self.person.firstName = json["personlista"]["person"][index]["tilltalsnamn"].stringValue
-                    self.person.gender = json["personlista"]["name"][index]["kon"].stringValue
-                    self.person.parti = json["personlista"]["person"][index]["parti"].stringValue
-                    self.person.status = json["personlista"]["person"][index]["status"].stringValue
-                    self.person.lastName = json["personlista"]["person"][index]["efternamn"].stringValue
-                    self.person.imageMax = json["personlista"]["person"][index]["bild_url_max"].stringValue
-                    self.person.yearOfBirth = json["personlista"]["person"][index]["fodd_ar"].intValue
-                    self.person.urlLink = json["personlista"]["person"][index]["bild_url_192"].stringValue
-                    self.person.valkrets = json["personlista"]["person"][index]["valkrets"].stringValue
-                    self.person.uppdrag = json["personlista"]["person"][index]["personuppdrag"]["uppdrag"][0]["roll_kod"].stringValue
-                    self.person.uppgift = json["personlista"]["person"][index]["personuppgift"]["uppgift"][0]["kod"].stringValue
-                    
-                    let newPerson = Person(firstName: self.person.firstName, lastName: self.person.lastName, yearOfBirth: self.person.yearOfBirth, parti: self.person.parti, urlLink: self.person.urlLink, gender: self.person.gender, status: self.person.status, imageMax: self.person.imageMax, valkrets: self.person.valkrets, uppdrag: self.person.uppdrag, uppgift: self.person.uppgift)
-                    self.personList.append(newPerson)
+                    let person = Person(json: json["personlista"]["person"][index])
+                    self.personList.append(person)
                 }
                 completion(self.personList, nil)
             case .failure(let error):
@@ -51,29 +39,29 @@ class  DataManager {
     }
     
     
-    func updatePersonsData(json : JSON, completion: @escaping ([Person]) -> Void) {
-        
-        for index in 0..<5{
-            person.firstName = json["personlista"]["person"][index]["tilltalsnamn"].stringValue
-            person.gender = json["personlista"]["name"][index]["kon"].stringValue
-            person.parti = json["personlista"]["person"][index]["parti"].stringValue
-            person.status = json["personlista"]["person"][index]["status"].stringValue
-            person.lastName = json["personlista"]["person"][index]["efternamn"].stringValue
-            person.imageMax = json["personlista"]["person"][index]["bild_url_max"].stringValue
-            person.yearOfBirth = json["personlista"]["person"][index]["fodd_ar"].intValue
-            person.urlLink = json["personlista"]["person"][index]["bild_url_192"].stringValue
-            person.valkrets = json["personlista"]["person"][index]["valkrets"].stringValue
-            person.uppdrag = json["personlista"]["person"][index]["personuppdrag"]["uppdrag"][0]["roll_kod"].stringValue
-            person.uppgift = json["personlista"]["person"][index]["personuppgift"]["uppgift"][0]["kod"].stringValue
-            
-            let newPerson = Person(firstName: person.firstName, lastName: person.lastName, yearOfBirth: person.yearOfBirth, parti: person.parti, urlLink: person.urlLink, gender: person.gender, status: person.status, imageMax: person.imageMax, valkrets: person.valkrets, uppdrag: person.uppdrag, uppgift: person.uppgift)
-            personList.append(newPerson)
-            
-            //            print("Uppdrag är: " , newPerson)
-            completion(personList)
-            
-        }
-    }
+//    func updatePersonsData(json : JSON, completion: @escaping ([Person]) -> Void) {
+//
+//        for index in 0..<5{
+//            person.firstName = json["personlista"]["person"][index]["tilltalsnamn"].stringValue
+//            person.gender = json["personlista"]["name"][index]["kon"].stringValue
+//            person.parti = json["personlista"]["person"][index]["parti"].stringValue
+//            person.status = json["personlista"]["person"][index]["status"].stringValue
+//            person.lastName = json["personlista"]["person"][index]["efternamn"].stringValue
+//            person.imageMax = json["personlista"]["person"][index]["bild_url_max"].stringValue
+//            person.yearOfBirth = json["personlista"]["person"][index]["fodd_ar"].intValue
+//            person.urlLink = json["personlista"]["person"][index]["bild_url_192"].stringValue
+//            person.valkrets = json["personlista"]["person"][index]["valkrets"].stringValue
+//            person.uppdrag = json["personlista"]["person"][index]["personuppdrag"]["uppdrag"][0]["roll_kod"].stringValue
+//            person.uppgift = json["personlista"]["person"][index]["personuppgift"]["uppgift"][0]["kod"].stringValue
+//
+//            let newPerson = Person(firstName: person.firstName, lastName: person.lastName, yearOfBirth: person.yearOfBirth, parti: person.parti, urlLink: person.urlLink, gender: person.gender, status: person.status, imageMax: person.imageMax, valkrets: person.valkrets, uppdrag: person.uppdrag, uppgift: person.uppgift)
+//            personList.append(newPerson)
+//
+//            //            print("Uppdrag är: " , newPerson)
+//            completion(personList)
+//
+//        }
+//    }
     
     
 }
--- RiksdagsMemo/Services/Data/Person.swift
+++ RiksdagsMemo/Services/Data/Person.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SwiftyJSON
 
 struct Person {
     var firstName : String = ""
@@ -29,4 +30,19 @@ struct Image {
     let image: UIImage = UIImage()
 }
 
+extension Person {
+    init(json: JSON) {
+        self.firstName = json["tilltalsnamn"].stringValue
+        self.gender = json["kon"].stringValue
+        self.parti = json["parti"].stringValue
+        self.status = json["status"].stringValue
+        self.lastName = json["efternamn"].stringValue
+        self.imageMax = json["bild_url_max"].stringValue
+        self.yearOfBirth = json["fodd_ar"].intValue
+        self.urlLink = json["bild_url_192"].stringValue
+        self.valkrets = json["valkrets"].stringValue
+        self.uppdrag = json["personuppdrag"]["uppdrag"][0]["roll_kod"].stringValue
+        self.uppgift = json["personuppgift"]["uppgift"][0]["kod"].stringValue
+    }
+}
 
